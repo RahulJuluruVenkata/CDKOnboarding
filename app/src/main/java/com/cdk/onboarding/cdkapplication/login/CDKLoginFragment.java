94a4eed0a99114ea440f7b304af447ad7224009c
RahulJuluruVenkata <jsrkprasadpnb@gmail.com>
2017-07-12 16:42:32 +0530

    Updated fragment structures for login screen

--- app/src/main/java/com/cdk/onboarding/cdkapplication/login/CDKLoginFragment.java
+++ app/src/main/java/com/cdk/onboarding/cdkapplication/login/CDKLoginFragment.java
@@ -6,18 +6,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.cdk.onboarding.cdkapplication.R;
-
-/**
- * Created by julurur on 12/07/17.
- */
+import com.cdk.onboarding.controller.LoginController;
 
 public class CDKLoginFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.cdk_login_fragment, container, false);
+
+        EditText txtUserName = (EditText) view.findViewById(R.id.txt_login);
+        EditText txtPassword = (EditText) view.findViewById(R.id.txt_password);
+        Button loginButton = (Button) view.findViewById(R.id.btn_login);
+
+        loginButton.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    LoginController loginController = new LoginController();
+                    if (loginController.isValidUser()) {
+                        buttonClicked(view);
+                    }
+                }
+
+                private void buttonClicked(View view) {
+
+                }
+            }
+        );
         return view;
     }
 }
---
+++ app/src/main/java/com/cdk/onboarding/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.cdk.onboarding.controller;
+
+/**
+ * Created by julurur on 12/07/17.
+ */
+
+public class LoginController {
+    public boolean isValidUser(){
+        return true;
+    }
+}
