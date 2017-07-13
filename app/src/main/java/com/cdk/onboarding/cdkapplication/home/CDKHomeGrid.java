d9e2ba0477bf596ce13df54f72dc0e9e952ee244
julurur <jsrkprasadpnb@gmail.com>
2017-07-13 19:39:19 +0530

    Added contextual menu for CDK home page

--- app/src/main/java/com/cdk/onboarding/cdkapplication/home/CDKHomeGrid.java
+++ app/src/main/java/com/cdk/onboarding/cdkapplication/home/CDKHomeGrid.java
@@ -4,6 +4,7 @@ import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.GestureDetector;
+import android.view.Menu;
 import android.view.MotionEvent;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +18,12 @@ public class CDKHomeGrid extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setHomeBannerText("Edited");
         setContentView(R.layout.activity_grid_cdk_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     //This gets called by top fragment when user clicks button
